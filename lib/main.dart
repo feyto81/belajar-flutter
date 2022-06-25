5023ac887e3526890fe8662fe37bbf71a33d6d7d
feyto81 <feyto81@gmail.com>
2022-06-25 22:33:58 +0700

    adding Container in widget text in body

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Aplikasi Hello World"),
         ),
-        body: Center(child: Text("Hello World")),
+        body: Center(child: Container(child: Text("Hello World"))),
       ),
     );
   }
